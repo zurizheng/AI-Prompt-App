7afd92668c24e94fe207758a9ad4f2e87922f885
zurizheng <zurizheng88@gmail.com>
2024-08-15 23:23:10 -0600

    created custom hook for refreshing data

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -7,45 +7,26 @@ import SearchInput from '@/components/SearchInput'
 import Trending from '@/components/Trending'
 import EmptyState from '@/components/EmptyState'
 import { getAllPosts } from '@/lib/appwrite'
+import useAppwrite from '@/lib/useAppwrite'
 
 const Home = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-
-      try {
-        const response = await getAllPosts();
-  
-        setData(response);
-      } catch (error) {
-        Alert.alert('Error', error.message)
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchData();
-  }, []);
-
-  console.log(data)
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
 
   const [refreshing, setRefreshing] = useState(false)
 
   const onRefresh = async () => {
     setRefreshing(true);
-    // call the videos again if any new videos appear
+    await refetch();
     setRefreshing(false);
   }
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{id: 1}, {id: 2}, {id: 3}]}
+        data={posts}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
+          <Text className="text-3xl text-white">{item.title}</Text>
         )}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
