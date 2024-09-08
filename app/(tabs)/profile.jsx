b8352a2fc2605bdb6eb2c28a8916b5b25c6ae485
zurizheng <zurizheng88@gmail.com>
2024-09-08 01:27:20 -0400

    finished profile page

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -10,10 +10,15 @@ import VideoCard from '@/components/VideoCard'
 import { useLocalSearchParams } from 'expo-router'
 import { useGlobalContext } from '@/context/GlobalProvider'
 import { icons } from '@/constants'
+import InfoBox from '@/components/InfoBox'
 
 const Profile = () => {
   const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
+
+  const logout = () => {
+
+  }
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -25,9 +30,36 @@ const Profile = () => {
         )}
         ListHeaderComponent={() => (
           <View className="w-full justify-center items-center mt-6 mb-12 px-4">
-            <TouchableOpacity className="w-full items-end mb-10">
+            <TouchableOpacity
+              className="w-full items-end mb-10"
+              onPress={logout}
+            >
               <Image source={icons.logout} resizeMode="contain" className="w-6 h-6" />
             </TouchableOpacity>
+            <View className="w-16 h-16 border border-secondary rounded-lg justify-center items-center">
+              <Image source={{ uri: user?.avatar }} className="w-[90%] h-[90%] rounded-lg" resizeMode="cover"/>
+            </View>
+
+            <InfoBox
+              title={user?.username}
+              containerStyles="mt-5"
+              titleStyles="text-lg"
+            />
+
+            <View className="mt-5 flex flex-row">
+              <InfoBox
+                title={posts.length || 0}
+                subtitle="Posts"
+                containerStyles="mr-10"
+                titleStyles="text-xl"
+              />
+
+              <InfoBox
+                title="1.2k"
+                subtitle="Followers"
+                titleStyles="text-xl"
+              />
+            </View>
           </View>
         )}
         ListEmptyComponent={() => (
