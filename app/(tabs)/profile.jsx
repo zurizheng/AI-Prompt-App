260459f7d71c0e1570a50cd38e7241e897933519
zurizheng <zurizheng88@gmail.com>
2024-09-08 00:42:46 -0400

    found out how to display video posted by user

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
+import React, { useEffect } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import SearchInput from '@/components/SearchInput'
+import EmptyState from '@/components/EmptyState'
+import { getUserPosts } from '@/lib/appwrite'
+import useAppwrite from '@/lib/useAppwrite'
+import VideoCard from '@/components/VideoCard'
+import { useLocalSearchParams } from 'expo-router'
+import { useGlobalContext } from '@/context/GlobalProvider'
+import { icons } from '@/constants'
 
 const Profile = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
+  const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard video={item} />
+        )}
+        ListHeaderComponent={() => (
+          <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+            <TouchableOpacity className="w-full items-end mb-10">
+              <Image source={icons.logout} resizeMode="contain" className="w-6 h-6" />
+            </TouchableOpacity>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos found for this search query"
+          />
+        )}
+      />
+    </SafeAreaView>
   )
 }
 
 export default Profile
-
-const styles = StyleSheet.create({})
